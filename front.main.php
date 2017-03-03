fdabfdd18664a6c440013f30177280bcdc14e3e7
Milos Zivojinovic <miloszivojinovic93@gmail.com>
2017-03-03 23:11:43 +0100

    footer updates from trello

--- front.main.php
+++ front.main.php
@@ -321,7 +321,7 @@ if( ! empty( $bg_File ) && $bg_File->exists() )
 			</div>
 		</div>
 
-		<footer class="col-md-12 footer_wrapper">
+		<footer class="col-md-12 footer_wrapper center">
 
 			<div class="evo_container evo_container__footer">
 			<?php
