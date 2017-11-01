48a133e00ee544aafc4c79c2a281bab9e0bb53c4
Francois Planque <github@fplanque.net>
2017-11-01 02:54:12 +0100

    Version bump

--- _skin.class.php
+++ _skin.class.php
@@ -21,7 +21,7 @@ class jared_Skin extends Skin
 	 * Skin version
 	 * @var string
 	 */
-	var $version = '1.0.2';
+	var $version = '7.0.2';
 
 	/**
 	 * Do we want to use style.min.css instead of style.css ?
